44121b1f61570163505ccc5a8b094cef111744bc
onlinevideos866-byte <onlinevideos866@gmail.com>
2026-08-21 20:55:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ def chat():
         client = genai.Client(api_key=api_key_fija)
         
         response = client.models.generate_content(
-            model='gemini-3.6-flash',
+            model='gemini-1.5-flash',
             contents=user_message,
             config={
-                "tools": [{"google_search": {}}],
                 "system_instruction": "Eres Bexia, una entidad digital autonoma creada por Fer."
             }
         )
